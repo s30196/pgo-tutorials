a9f969b951324e107beab62201a042064fff5814
Andrey Kazlov <s30196@pjwstk.edu.pl>
2024-03-05 11:13:29 +0100

    Exercise 1

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class  Main {
+
+    public static void main(String [] args) {
+
+        Scanner a = new Scanner(System.in);
+
+        int b = a.nextInt();
+
+        if (b>0) {
+            System.out.println("Positive");
+        }else if (b<0) {
+            System.out.println("NEGATIVE");
+        }else {
+            System.out.println("Zero");
+        }
+    }
+
+}
