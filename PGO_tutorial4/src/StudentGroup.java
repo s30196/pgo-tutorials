2ad4b8e728306fb390487676fa92be89d889ffed
s30196 <s30196@pjwstk.edu.pl>
2024-03-26 10:42:32 +0100

    tutorial4

--- PGO_tutorial4/src/StudentGroup.java
+++ PGO_tutorial4/src/StudentGroup.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class StudentGroup {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentGroup {
+    public String name;
+    public List<Student> students = new ArrayList<>();
+
+    public StudentGroup(String name) {
+        this.name = name;
+    }
+
+    public void add(Student s1) {
+
+    }
+
+    // implement a method for adding students to the group
+    // .add method for Lists
 }
